2a8875a899ddd27c4234c999715df1d869b4942b
ciec20151104680 <a20151104680@mac22.local>
2018-07-05 20:13:25 +0800

    update
    更新背景等

--- RunningPanda/RunningPanda/GameViewController.swift
+++ RunningPanda/RunningPanda/GameViewController.swift
@@ -6,50 +6,63 @@
 //  Copyright © 2018年 20151104680. All rights reserved.
 //
 
+
+
 import UIKit
 import SpriteKit
-import GameplayKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: "sks")
+        
+        let sceneData = try? NSData(contentsOfFile: path!, options: NSDataReadingOptions.DataReadingMappedIfSafe)
+        //dataWithContentsOfFile(path!, options: .DataReadingMappedIfSafe, error: nil)
+        let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData!)
+        
+        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+        let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+        archiver.finishDecoding()
+        return scene
+    }
+}
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
             
-            view.ignoresSiblingOrder = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
         }
     }
-
-    override var shouldAutorotate: Bool {
+    
+    override func shouldAutorotate() -> Bool {
         return true
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return UIInterfaceOrientationMask.AllButUpsideDown
         } else {
-            return .all
+            return UIInterfaceOrientationMask.All
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    
 }
