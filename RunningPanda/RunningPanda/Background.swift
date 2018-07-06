52564d45533ae7bcf0aebd4f14f6eabe29c0a644
ciec20151104680 <a20151104680@mac22.local>
2018-07-06 15:16:11 +0800

    platAndapple
    平台类 和 苹果类

--- RunningPanda/RunningPanda/Background.swift
+++ RunningPanda/RunningPanda/Background.swift
@@ -86,3 +86,4 @@ class Background:SKNode {
     }
     
 }
+
---
+++ RunningPanda/RunningPanda/BitMaskType.swift
@@ -0,0 +1,22 @@
+//
+//  BitMaskType.swift
+//  RunningPanda
+//
+//  Created by 20151104680 on 18/7/6.
+//  Copyright © 2018年 20151104680. All rights reserved.
+//
+
+class BitMaskType {
+    class var panda:UInt32 {
+        return 1<<0
+    }
+    class var platform:UInt32 {
+        return 1<<1
+    }
+    class var apple:UInt32 {
+        return 1<<2
+    }
+    class var scene:UInt32{
+        return 1<<3
+    }
+}
